17a47e7a353aa32cf22c98376d106acd0c3fa4e3
juliastella <juliastellasil1@gmai.com>
2022-11-20 22:12:59 -0300

    Perto da conclusão das classes

--- classe.py
+++ classe.py
@@ -11,23 +11,31 @@ class Conta_corrente():
     def depositar(self, valorDeposito):
         self.saldoCorrente += valorDeposito
 
-"""    def aplicar(self, ):"""
+    def aplicar(self,investimentoIn):
+        self.saldoCorrente -= investimentoIn
 
 
 
-valorDeposito = float(input("Qual é o deposito? "))
+# Area de teste para verificar o funcionamento das classe:
+"""valorDeposito = float(input("Qual é o deposito? "))
+
+investimentoIn = float(input("Qual é o investimento? "))
 
 pessoa1 = Conta_corrente(1234,"júlia", 100)
 pessoa1.depositar(valorDeposito)
 pessoa1.sacar(valorDeposito)
+pessoa1.aplicar(investimentoIn)
 
-print(pessoa1.saldoCorrente)
-
-"""class Conta_poupaca(Conta_corrente):
+print(pessoa1.saldoCorrente)"""
+# Area de teste para verificar o funcionamento das classe:
+class Conta_poupaca(Conta_corrente):
     def __init__(self,saldoPoupaca,numeroConta,nomeTitular):
-        super().__init__(numeroConta, nomeTitular)
+        super().__init__(numeroConta, nomeTitular) #erro no parentes;
         self.saldoPoupaca = saldoPoupaca
-        pass
-"""
-"""def resgatar(self, ):"""
-"""def transferir(self, ):"""
+
+    def resgatar(self):
+        self.saldoPoupaca -= self.saldoCorrente
+
+pessoa2 = Conta_poupaca(1234,"júlia", 50)
+pessoa2.resgatar()
+print(pessoa2.saldoCorrente, "E", pessoa2.saldoPoupaca)
--- condicionais.py
+++ condicionais.py
@@ -3,3 +3,24 @@ import random
 
 # Vai ser feito o menu e todas as condicionais:
 
+# Armazenamento de dados:
+dicionario : {}
+nome = input("Digite seu nome: ")
+
+print("Bem Vindo ao Senac Bank")
+print(""" 
+
+        +------------------------------+
+        |         -Senac Bank-         | 
+        |                              |
+        | [1] Criar conta              |
+        | [2] Mostrar dados            |
+        | [3] Acessar Conta Corrente   | 
+        | [4] Acessar Conta Poupança   |
+        | [5] Sair                     |
+        |                              | 
+        +-------------------------------+                               
+                                         \n""")
+
+#
+
